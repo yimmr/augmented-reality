92638420c7f566158e1a8d3ed377cbf4ed0971e9
imoncn <service@yimmr.com>
2023-07-17 03:38:16 +0800

    first commit

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <?php
 /*
 Plugin Name: Augmented reality plugin
-Plugin URI: 
-Description: Marked based augmented reality in your wordpress https site  
+Plugin URI:
+Description: Marked based augmented reality in your wordpress https site
 Version: 1.2.0
 Author: Deimos Mavrozoumis
 Author URI: https://plugfame.wordpress.com
 */
 
-if (!defined('ABSPATH'))
-{
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
+add_action('template_redirect', function () {
+    if (isset($_GET['art'])) {
+        $arType = $_GET['art'] ?: 1;
+        require_once 'a.php';
+        exit;
+    }
+});
+
 define('PL_AR_PATH', plugin_dir_path(__FILE__));
 define('PL_AR_LINK', plugin_dir_url(__FILE__));
-//include_once PL_AR_PATH . 'classes/new_page.php';
-include_once PL_AR_PATH . 'classes/shortcode.php';
-include_once PL_AR_PATH . 'classes/utilities.php';
+// include_once PL_AR_PATH . 'classes/new_page.php';
+include_once PL_AR_PATH.'classes/shortcode.php';
+include_once PL_AR_PATH.'classes/utilities.php';
 
-$pl_ar_utilities= new  pl_ar_utilities\pl_ar_Utilities();
-$pl_ar_shortcode= new pl_ar_shortcode\pl_ar_Shortcode();
-//$pl_ar_new_page= new pl_ar_new_page\pl_ar_new_page();
+$pl_ar_utilities = new pl_ar_utilities\pl_ar_Utilities();
+$pl_ar_shortcode = new pl_ar_shortcode\pl_ar_Shortcode();
+// $pl_ar_new_page= new pl_ar_new_page\pl_ar_new_page();
 
-register_activation_hook(   __FILE__, array( 'pl_ar_utilities\pl_ar_Utilities', 'pl_ar_table_activation' ) );
-register_deactivation_hook(   __FILE__, array( 'pl_ar_utilities\pl_ar_Utilities', 'pl_ar_table_deactivation' ) );
-register_activation_hook( __FILE__, array( 'pl_ar_utilities\pl_ar_Utilities', 'pl_ar_plugin_activation' ) );
-register_deactivation_hook( __FILE__, array( 'pl_ar_utilities\pl_ar_Utilities', 'pl_ar_plugin_deactivation' ) );
+register_activation_hook(__FILE__, ['pl_ar_utilities\pl_ar_Utilities', 'pl_ar_table_activation']);
+register_deactivation_hook(__FILE__, ['pl_ar_utilities\pl_ar_Utilities', 'pl_ar_table_deactivation']);
+register_activation_hook(__FILE__, ['pl_ar_utilities\pl_ar_Utilities', 'pl_ar_plugin_activation']);
+register_deactivation_hook(__FILE__, ['pl_ar_utilities\pl_ar_Utilities', 'pl_ar_plugin_deactivation']);
