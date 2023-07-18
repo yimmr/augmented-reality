034ef9dbb9a2c06670d674a51b3a4e51891c851f
imoncn <service@yimmr.com>
2023-07-18 18:25:50 +0800

    first commit

--- views/image.php
+++ views/image.php
@@ -3,7 +3,8 @@
     <?php require_once __DIR__.'/assets.php'; ?>
     <a-nft type='nft' url='<?php echo $markerURL; ?>' smooth='true'
            smoothCount='10' smoothTolerance='0.01' smoothThreshold='5'>
-        <?php require_once __DIR__.'/show-object.php'; ?>
+        <?php // require_once __DIR__.'/show-object.php';?>
+        <a-box position='0 0.5 0' color="yellow"></a-box>
     </a-nft>
     <a-entity camera></a-entity>
 </a-scene>
