7e2c607507fc5bcb820fbd3071a3c70db58fe152
imoncn <service@yimmr.com>
2023-07-19 03:58:54 +0800

    first commit

--- views/image.php
+++ views/image.php
@@ -1,9 +1,11 @@
+<script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1.3.0/dist/aframe-master.min.js"></script>
+<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
 <a-scene vr-mode-ui='enabled: false;' renderer="logarithmicDepthBuffer: true; precision: medium;" embedded
          arjs='trackingMethod: best; sourceType: webcam; debugUIEnabled: false;'>
     <?php require_once __DIR__.'/assets.php'; ?>
     <?php foreach ($pageData['items'] as $item) {?>
     <a-nft type="nft"
-           url="<?php echo $item['marker']['url']; ?>"
+           url="https://ar-js-org.github.io/.github/profile/aframe/examples/image-tracking/nft/trex/trex-image/trex"
            smooth="true" smoothCount="10" smoothTolerance=".01" smoothThreshold="5">
         <?php echo $arPage->buildObjectHTML($item['object']); ?>
     </a-nft>
